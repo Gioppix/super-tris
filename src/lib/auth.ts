96445af54f8994c909422311c8f002abf10ba3bb
Giovanni <gfeltrin25@gmail.com>
2025-10-23 19:00:16 +0200

    Added `drizzle` and auto-migrations

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,9 +2,27 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SQLITE_PATH } from '$env/static
 import { betterAuth } from 'better-auth';
 import { anonymous } from 'better-auth/plugins';
 import Database from 'better-sqlite3';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { mkdirSync } from 'fs';
+import { dirname } from 'path';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import * as schema from '../../migrations/auth-schema';
+
+const DB_NAME = 'sqlite.db';
+mkdirSync(dirname(SQLITE_PATH), { recursive: true });
+
+const sqlite = new Database(`${SQLITE_PATH}/${DB_NAME}`);
+const db = drizzle({ client: sqlite, schema });
+migrate(db, {
+    migrationsFolder: './drizzle'
+});
 
 export const auth = betterAuth({
-    database: new Database(SQLITE_PATH),
+    database: drizzleAdapter(db, {
+        provider: 'sqlite',
+        schema
+    }),
     socialProviders: {
         google: {
             clientId: GOOGLE_CLIENT_ID,
